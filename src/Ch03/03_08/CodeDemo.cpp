63c3ba46287cb42ec55c20f41cb5c3b263ce81de
mbaker4040-max <mbaker4040@gmail.com>
2026-01-03 22:19:29 +0000

    updated comments

--- src/Ch03/03_08/CodeDemo.cpp
+++ src/Ch03/03_08/CodeDemo.cpp
@@ -8,7 +8,8 @@
 double my_average(int a, int8_t b, uint32_t c){
     double result = 0.0;
 
-    // Write your code here
+    result = (int)a + (int)b + (int)c; // Calculating the average
+    result = result / 3.0; //Additional answer (a + b + c)/3.0
     
     return result;
 }
