bdf826d86373e640c01884850421fa24fe30a296
mbaker4040-max <mbaker4040@gmail.com>
2026-01-03 02:10:13 +0000

    added code from lesson

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,13 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+// reminder on how to code and in C++
+
+#include <iostream>
+
+int main (){
+    std:: cout << "Hi there!" << std::endl;
+
+    std:: cout << std::endl << std::endl;
+    return 0;
+}
