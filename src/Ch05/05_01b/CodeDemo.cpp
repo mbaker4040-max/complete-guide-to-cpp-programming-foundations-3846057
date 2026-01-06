0278b07401ead1796ef3102f65b8e007e5ed5695
mbaker4040-max <mbaker4040@gmail.com>
2026-01-06 00:07:14 +0000

    Completed section

--- src/Ch05/05_01b/CodeDemo.cpp
+++ src/Ch05/05_01b/CodeDemo.cpp
@@ -9,6 +9,24 @@ int main(){
     bool gamePaused = false;
     char action = 'x';
     
+    if (score > 1000)
+        std::cout << "Your score is over 1000." << std::endl;
+
+    if (score % 2)
+        std::cout << "Score is odd" << std::endl;
+    else
+        std::cout << "Score is even" <<std::endl;    
+    std::cout << "The action '" << action << "' is ";
+    if (action != 'a' && action != 'w' && action != 'd' && action != 's' &&
+        action != 'A' && action != 'W' && action != 'D' && action != 'S')
+        std::cout << "not ";
+    std::cout << "a mvoment key." << std::endl;
+
+    if (gamePaused)
+        std::cout << "The game is paused!" << std::endl;
+    else
+        std::cout << "The game is running!" << std::endl;    
+
     std::cout << std::endl << std::endl;
     return 0;
 }
